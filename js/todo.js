8bde3ade5e65a4b6641c0f28eb26a44260e4c24b
wkhearn <wkhearn@gmail.com>
2017-07-09 22:23:44 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  // call functions here
-});
+  submitFormEvent()
+})
 
-// define functions here
+function submitFormEvent(){
+  document.querySelector("form").addEventListener("submit", function(event){
+    event.preventDefault()
+    var listItem = document.querySelector('#item').value
+    document.querySelector('ol').innerHTML += ( '<li>' + listItem + '</li>' )
+  })
+}
+// function submitFormEvent(){
+//   $('form').on('submit', function(event){
+//     var listItem = $('#item').val();
+//     $('ol').append( '<li>' + listItem + '</li>' );
+//     event.preventDefault();
+//   });
+// }
